80462cce9ce9c7af70554f7785f7287820a466d8
Alex Schiffer <aschiffer186@gmail.com>
2022-10-18 20:58:09 -0500

    Add syntax tree base

---
+++ Nikolac/Syntax-Analysis/include/SyntaxTree/SyntaxNode.hxx
@@ -0,0 +1,27 @@
+#ifndef SYNTAX_NODE_HH
+#define SYNTAX_NODE_HH
+
+#include <vector>
+
+namespace Nikola::SyntaxAnalysis
+{
+    enum class SyntaxNodeType
+    {
+
+    };
+
+    struct Visitor;
+
+    struct SyntaxNode
+    {
+        virtual SyntaxNodeType getNodeType() const = 0;
+
+        virtual std::vector<SyntaxNode*> getChildren() const = 0;
+
+        virtual void accept(Visitor* visitor) = 0;
+
+        virtual ~SyntaxNode() = default;
+    };
+}
+
+#endif
